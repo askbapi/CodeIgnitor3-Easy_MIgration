fc5efedf774813c8a8b0796e66d88ed0ef373369
bapi <mail2bapi@astrosoft.co.in>
2017-11-09 20:32:56 +0530

    createJsonFile method can save table attributesto JSON file

--- Easy_migration.php
+++ Easy_migration.php
@@ -24,11 +24,66 @@ class Easy_migration
      */
     public function migrate($tableName='', $version=0)
     {
+        if((empty($tableName)) && ($version <= 0)){
+            $this->runForLatest();
+        }else{
+            $this->runfo();
+        }
+    }
 
+    private function runFor($className='', $version=0){
+
+        if($version > 0){
+            // Get the respective JSON file with version number
+            $file = self::BASEPATH.'json/'.str_pad($version, 3, "0", STR_PAD_LEFT).'_'.$className.'.json';
+
+            if(!file_exists($file)){
+                throw new Exception("JSON file does not exist.");
+            }
+
+            // Get Field Array from JSON file
+            $fields = json_decode(file_get_contents($file));
+
+            // Get class name as Table name & Create Table
+            $this->dbforge->add_field($fields);
+
+            $tableAttributes = [
+                'ENGINE' => $object->storageEngine,
+                'CHARACTER SET' => $object->charset,
+                'COLLATE' => $object->collation,
+            ];
+            $this->dbforge->create_table($this->tableName, true, $tableAttributes);
+        }else{
+            // Get data from migration class
+            $file = self::BASEPATH.$className.'.php';
+            $objectClassName = ucfirst($className);
+            $object = new $objectClassName;
+
+            // Execute the fields method
+            $object->fields();
+
+            // Collect Array
+            $fields = $object->getFields();
+
+            // Get Table Attributes
+            $tableAttributes = [
+                'ENGINE' => $object->storageEngine,
+                'CHARACTER SET' => $object->charset,
+                'COLLATE' => $object->collation,
+            ];
+
+            # Create JSON file under migrate directory with incremental value
+            $this->createJsonFile($fields, $objectClassName);
+
+            # Get class name as Table name & Create Table
+            $this->dbforge->add_field($fields);
+
+
+            $this->dbforge->create_table($this->tableName, true, $tableAttributes);
+        }
 
 
     }
-
 
     /**
      * Run when migration run for Latest file
@@ -49,16 +104,16 @@ class Easy_migration
             $fields = $object->getFields();
 
             # Create JSON file under migrate directory with incremental value
-            $this->createJsonFile($fields, $objectClassName);
+            $this->createJsonFile(
+                $fields,
+                $objectClassName,
+                $this->getTableAttributes($object)
+                );
 
             # Get class name as Table name & Create Table
             $this->dbforge->add_field($fields);
 
-            $tableAttributes = [
-                'ENGINE' => $object->storageEngine,
-                'CHARACTER SET' => $object->charset,
-                'COLLATE' => $object->collation,
-            ];
+
             $this->dbforge->create_table($this->tableName, true, $tableAttributes);
         }
     }
@@ -68,8 +123,9 @@ class Easy_migration
      * Save fields into JSON file with incremented version number
      * @param array $fields
      * @param string $className
+     * @param array $tableAttributes
      */
-    private function createJsonFile(array $fields, $className='')
+    private function createJsonFile(array $fields, $className='', array $tableAttributes)
     {
         // Search for last File
         $jsonFiles = array_diff(
@@ -94,8 +150,28 @@ class Easy_migration
         $newVersion = $latestVersion + 1;
         $newFile = self::BASEPATH.'json/'.str_pad($newVersion, 3, "0", STR_PAD_LEFT).'_'.strtolower($className).'.json';
 
-        $jsonData = json_encode($fields);
+        $jsonData = json_encode(
+            [
+                'fields'=> $fields,
+                'tableAttributes' => $tableAttributes
+            ]
+        );
 
         file_put_contents($newFile, $jsonData);
+    }
+
+
+    /**
+     * Collect table attributes from migration object and return as array
+     * @param $object
+     * @return array
+     */
+    private function getTableAttributes($object)
+    {
+        return [
+            'ENGINE' => $object->storageEngine,
+            'CHARACTER SET' => $object->charset,
+            'COLLATE' => $object->collation,
+        ];
     }
 }
